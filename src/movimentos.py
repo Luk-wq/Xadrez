f32be2231a3b9653cab7990df00ffd32222bc1fc
Luk-wq <luk.ksograf@gmail.com>
2025-07-06 02:38:34 -0300

    Create movimentos.py

---
+++ src/movimentos.py
@@ -0,0 +1,4 @@
+def movimento_valido(origem_linha, origem_coluna, destino_linha, destino_coluna):
+    if origem_linha == destino_linha and origem_coluna == destino_coluna:
+        return False
+    return True
