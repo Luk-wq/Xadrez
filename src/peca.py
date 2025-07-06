7c9f12a4bc46e9dec2f95bbe8222050c78a6fd54
Luk-wq <luk.ksograf@gmail.com>
2025-07-06 02:41:10 -0300

    Update peca.py

--- src/peca.py
+++ src/peca.py
@@ -1,1 +1,7 @@
+class Peca:
+    def __init__(self, tipo, cor):
+        self.tipo = tipo  # 'P', 'T', 'C', etc.
+        self.cor = cor    # 'branco' ou 'preto'
 
+    def __str__(self):
+        return self.tipo
